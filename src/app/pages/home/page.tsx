7a7a392eb95bf8477ee93d0a0f1150eaa4c861b1
vaxo <vaxojaniashvili186@gmail.com>
2024-10-17 15:20:48 +0400

    finished-header/UI&left-nav-UI/functional

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -2,25 +2,42 @@ import FacebookIcon from "@/app/common/components/_atoms/assets/FacebookIcon";
 import GithubIcon from "@/app/common/components/_atoms/assets/GithubIcon";
 import LinkeDinIcon from "@/app/common/components/_atoms/assets/LinkedinIcon";
 import YoutubeIcon from "@/app/common/components/_atoms/assets/YoutubeIcon";
-import Image from "next/image";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div className="flex w-full h-screen">
       <div className="flex justify-start w-1/2 items-center">
         <div className="px-1 sm:hidden md:hidden lg:flex xl:flex 2xl:flex flex-col justify-between py-3 h-[275px] bg-white rounded">
-          <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
-            <LinkeDinIcon className="w-[30px] h-[30px]" />
-          </div>
-          <div className=" py-4 flex justify-center hover:bg-[#7843e933] cursor-pointer rounded-lg">
-            <YoutubeIcon className="w-[30px] h-[30px]" />
-          </div>
-          <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
-            <GithubIcon className="w-[30px] h-[30px]" />
-          </div>
-          <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
-            <FacebookIcon className="w-[30px] h-[30px]" />
-          </div>
+          <Link
+            target="_blank"
+            href="https://www.linkedin.com/in/vaxojaniashvili/"
+          >
+            <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
+              <LinkeDinIcon className="w-[30px] h-[30px]" />
+            </div>
+          </Link>
+          <Link
+            target="_blank"
+            href="https://www.youtube.com/@Vaxo-Janiashvili"
+          >
+            <div className=" py-4 flex justify-center hover:bg-[#7843e933] cursor-pointer rounded-lg">
+              <YoutubeIcon className="w-[30px] h-[30px]" />
+            </div>
+          </Link>
+          <Link target="_blank" href="https://github.com/vaxojaniashvili">
+            <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
+              <GithubIcon className="w-[30px] h-[30px]" />
+            </div>
+          </Link>
+          <Link
+            target="_blank"
+            href="https://www.facebook.com/vaxo.janiashvilii"
+          >
+            <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
+              <FacebookIcon className="w-[30px] h-[30px]" />
+            </div>
+          </Link>
         </div>
       </div>
       <div className="flex justify-center w-1 items-center">
