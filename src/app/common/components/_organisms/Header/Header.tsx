cca4bf009ee43f873b3d9b21885cf28935fd2753
vaxo <vaxojaniashvili186@gmail.com>
2024-10-27 23:24:33 +0400

    finished header and home page UI
    finished header with responsive and framer motion & also finished home page

--- src/app/common/components/_organisms/Header/Header.tsx
+++ src/app/common/components/_organisms/Header/Header.tsx
@@ -43,6 +43,8 @@ const Header = () => {
                     initial={{ opacity: 0, y: -50, scale: 0.9 }}
                     animate={{ opacity: 1, y: 0, scale: 1 }}
                     exit={{ opacity: 0, y: -50, scale: 0.9 }}
+
+                    
                     transition={{
                       type: "spring",
                       stiffness: 100,
