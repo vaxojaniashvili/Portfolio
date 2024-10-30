5462445afe1fa5875e59e7eb27450068437e1a9b
vaxo <vaxojaniashvili186@gmail.com>
2024-10-30 20:09:48 +0400

    fixed-vercel-error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import AboutPage from "./pages/about/page";
 import ContactPage from "./pages/contact/page";
 import Footer from "./pages/footer/page";
 import HomePage from "./pages/home/page";
-import ProjectsPage from "./pages/projects/page";
+// import ProjectsPage from "./pages/projects/page"; esec aq
 import { motion, useAnimation } from "framer-motion";
 import {ReactNode, useEffect} from "react";
 import { useInView } from "react-intersection-observer";
