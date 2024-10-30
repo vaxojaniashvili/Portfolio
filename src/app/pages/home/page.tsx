62dc26a56b4f6312e07bdb75bc47d1f019cd4a81
vaxo <vaxojaniashvili186@gmail.com>
2024-10-30 16:41:34 +0400

    test

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div id="home" className="flex w-full md:h-screen mb-[40px] mt-[100px] md:mb-0 md:mt-0 relative">
+    <div id="home" className="flex w-full md:h-screen mb-[40px] py-[40px] mt-[100px] md:mb-0 md:mt-0 relative">
       <div className="flex justify-start w-0 sm:w-1/2 items-center">
         <div className="px-1 hidden lg:flex flex-col justify-between py-3 h-[275px] bg-white rounded">
           <Link
@@ -43,7 +43,7 @@ const HomePage = () => {
       </div>
       <div className="flex justify-center w-full sm:w-1 mx-auto sm:mx-0 px-6 sm:px-0 items-center">
       <div className="text-center flex flex-col gap-y-[20px] md:gap-y-[30px]">
-          <h1 className="text-black font-bold text-[20px] sm:text-[30px] md:text-[50px] xl:text-[64px] whitespace-nowrap">
+          <h1 className="text-black font-bold text-[25px] sm:text-[30px] md:text-[50px] xl:text-[64px] whitespace-nowrap">
             Hey, I am VAXO JANIASHVILI
           </h1>
           <p className="lg:text-[1.2rem]">
