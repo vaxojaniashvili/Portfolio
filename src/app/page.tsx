ec56fa3174313ddb45b1b4541902b18e579399cf
vaxo <vaxojaniashvili186@gmail.com>
2024-10-30 20:07:47 +0400

    removed-framer-motion-project-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,9 +57,9 @@ const Page = () => {
             <Section>
                 <AboutPage />
             </Section>
-            <Section>
+            {/*<Section>* aq davakomentare aq mitoro projectebis peijze ar mushaobs kargad da gasasworebelia axla agaraq framer-motion am divs/}
                 <ProjectsPage />
-            </Section>
+            {/*</Section>*/}
             <Section>
                 <ContactPage />
             </Section>
