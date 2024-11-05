fa831bef277ccb641491da6ad17dd89e6069713b
vaxojaniashvili <vaxojaniashvili186@gmail.com>
2024-11-05 14:21:29 +0400

    projects-page-finished and contacts-page-UI

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -7,42 +7,45 @@ import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div id="home" className="flex w-full md:h-screen mb-[40px] mt-[100px] md:mb-0 md:mt-0 relative">
+    <div
+      id="home"
+      className="flex w-full md:h-screen mb-[40px] mt-[100px] md:mb-0 md:mt-0 relative"
+    >
       <div className="flex justify-start w-0 sm:w-1/2 items-center">
         <div className="px-1 hidden lg:flex flex-col justify-between py-3 h-[275px] bg-white rounded">
           <Link
-              target="_blank"
-              href="https://www.linkedin.com/in/vaxojaniashvili/"
+            target="_blank"
+            href="https://www.linkedin.com/in/vaxojaniashvili/"
           >
             <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
-              <LinkeDinIcon className="w-[30px] h-[30px]"/>
+              <LinkeDinIcon className="w-[30px] h-[30px]" />
             </div>
           </Link>
           <Link
-              target="_blank"
-              href="https://www.youtube.com/@Vaxo-Janiashvili"
+            target="_blank"
+            href="https://www.youtube.com/@Vaxo-Janiashvili"
           >
             <div className=" py-4 flex justify-center hover:bg-[#7843e933] cursor-pointer rounded-lg">
-              <YoutubeIcon className="w-[30px] h-[30px]"/>
+              <YoutubeIcon className="w-[30px] h-[30px]" />
             </div>
           </Link>
           <Link target="_blank" href="https://github.com/vaxojaniashvili">
             <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
-              <GithubIcon className="w-[30px] h-[30px]"/>
+              <GithubIcon className="w-[30px] h-[30px]" />
             </div>
           </Link>
           <Link
-              target="_blank"
-              href="https://www.facebook.com/vaxo.janiashvilii"
+            target="_blank"
+            href="https://www.facebook.com/vaxo.janiashvilii"
           >
             <div className="px-2.5 py-4 hover:bg-[#7843e933] cursor-pointer rounded-lg">
-              <FacebookIcon className="w-[30px] h-[30px]"/>
+              <FacebookIcon className="w-[30px] h-[30px]" />
             </div>
           </Link>
         </div>
       </div>
       <div className="flex justify-center w-full sm:w-1 mx-auto sm:mx-0 px-6 sm:px-0 items-center">
-      <div className="text-center flex flex-col gap-y-[20px] md:gap-y-[30px]">
+        <div className="text-center flex flex-col gap-y-[20px] md:gap-y-[30px]">
           <h1 className="text-black font-bold text-[25px] sm:text-[30px] md:text-[50px] xl:text-[64px] whitespace-nowrap">
             Hey, I am VAXO JANIASHVILI
           </h1>
@@ -57,11 +60,11 @@ const HomePage = () => {
           </div>
         </div>
         <Image
-            width={25}
-            height={40}
-            className="absolute w-[25px] h-[40px] bottom-0 bg-transparent rounded-full hidden lg:flex"
-            src="https://cdn.dribbble.com/users/1459765/screenshots/3563580/scrolling_mousewheel.gif"
-            alt="img-gif"
+          width={25}
+          height={40}
+          className="absolute w-[25px] h-[40px] bottom-0 bg-transparent rounded-full hidden lg:flex"
+          src="https://cdn.dribbble.com/users/1459765/screenshots/3563580/scrolling_mousewheel.gif"
+          alt="img-gif"
         />
       </div>
     </div>
