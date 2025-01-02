883007946514721dd0ee779a3d790076cfecfd6d
vaxojaniashvili <vaxojaniashvili186@gmail.com>
2025-01-02 19:59:52 +0400

    clean-code

--- src/app/common/types/common.ts
+++ src/app/common/types/common.ts
@@ -11,3 +11,18 @@ export interface IconProps {
   className?: string;
   view?:string
 }
+export interface ButtonProps {
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+  children: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
+}
+export interface InputProps {
+  className?: string;
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: 'text' | 'password' | "email";
+  id?: string;
+  required?: boolean;
+}
