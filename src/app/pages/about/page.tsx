266debf27c5eb83e0ebbdb3259c68c2825d0e91f
vaxojaniashvili <vaxojaniashvili186@gmail.com>
2025-01-12 12:57:12 +0400

    UI-Correct

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -75,7 +75,7 @@ const AboutPage = () => {
                     <h2 className={`text-2xl ${darkMode ? "text-[#F5F5F5] font-normal" : "text-gray-700"} font-bold mb-4`}>My Skills</h2>
                     <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
                         {['HTML', 'CSS', 'JavaScript',"Typescript", 'React', 'Next',"Firebase", "Node.js", "express.js", "nest.js", 'PHP', "Laravel", 'SASS', 'GitHub', 'Git',"Docker", 'SEO', "Linux", 'Terminal',"\n" +
-                        "MongoDB","REST-API","GraphQL","Jira",].map(skill => (
+                        "SQL","MongoDB","REST-API","GraphQL","Jira",].map(skill => (
                             <div
                                 key={skill}
                                 className={`px-4 py-2 ${darkMode ? "bg-gray-700 text-gray-200" : "bg-gray-200 text-gray-700"} rounded text-center font-semibold hover:rounded-2xl hover:bg-violet-700 cursor-pointer hover:text-white transition-all duration-500`}
